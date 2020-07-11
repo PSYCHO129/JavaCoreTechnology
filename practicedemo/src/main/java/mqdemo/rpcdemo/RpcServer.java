27c751ffc325f6d4a1da9a6ac6fa1461caf15fe3
zeng-s-h <zengsh625362190@163.com>
2020-07-11 15:32:29 +0800

    线程池相关知识

---
+++ practicedemo/src/main/java/mqdemo/rpcdemo/RpcServer.java
@@ -0,0 +1,62 @@
+package mqdemo.rpcdemo;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import mqdemo.RabbitMqUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author 小白i
+ * @date 2020/7/8
+ */
+public class RpcServer {
+
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+    public static void main(String[] args) throws Exception {
+
+        Channel channel = RabbitMqUtils.getChannel();
+
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+        channel.basicQos(1);
+        System.out.println(" [x] Awaiting RPC requests ");
+
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                //super.handleDelivery(consumerTag, envelope, properties, body);
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties().builder()
+                        .correlationId(properties.getCorrelationId())
+                        .build();
+                String response = "";
+                try {
+                    String message = new String(body, StandardCharsets.UTF_8);
+                    int n = Integer.parseInt(message);
+                    System.out.println("[.] fib( " + message + ")");
+                    response += fib(n);
+                } catch (Exception e) {
+                    System.out.println("[.]" + e.toString());
+                    e.printStackTrace();
+                } finally {
+                    channel.basicPublish("", properties.getReplyTo(), replyProps, response.getBytes(StandardCharsets.UTF_8));
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+    }
+
+    private static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
